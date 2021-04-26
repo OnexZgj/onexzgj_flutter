af1fcef9e1afd0ff5d4b65024a8b4d317a844017
onexzgj <474835552@qq.com>
2021-04-26 08:40:37 +0800

    初始提交，体验项目的listView的使用，及sizebox,相当于是占位的view,listView一定要设置展示的数量

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,57 @@
 import 'package:flutter/material.dart';
+import './model/post.dart';
 
 void main() {
-  runApp( App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Onexzgj flutter"),
-        ),
-        body: Hello(),
+      home: Home(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _itemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Image.network(posts[index].imageUrl),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(posts[index].author, style: Theme.of(context).textTheme.subhead),
+          //相当于是一个占位的View
+          // SizedBox(height: 5.0),
+
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('Onexzgj flutter'),
+      ),
+      body: ListView.builder(
+        itemBuilder: _itemBuilder,
+        itemCount: posts.length, //一定要指定这个数量，不然会出现显示半个屏幕
       ),
     );
   }
 }
 
-class Hello extends StatelessWidget{
+class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -28,10 +61,8 @@ class Hello extends StatelessWidget{
         style: TextStyle(
             fontSize: 40.0,
             fontWeight: FontWeight.bold,
-            color: Colors.deepPurple
-        ),
+            color: Colors.deepPurple),
       ),
     );
   }
-
 }
---
+++ lib/model/post.dart
@@ -0,0 +1,85 @@
+class Post {
+  const Post({this.title, this.author, this.imageUrl});
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Childhood in a picture',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Contained',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Free Hugs',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Gravity Falls',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Icecream Truck',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'keyclack',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Overkill',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Say Hello to Barry',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Space Skull',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'The Old Fashioned',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Tornado',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'Undo',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  ),
+  Post(
+    title: 'White Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://img9.51tietu.net/pic/2019-091302/m3rlpw5wx0dm3rlpw5wx0d.jpg',
+  )
+];
